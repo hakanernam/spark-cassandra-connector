cd1afc4859c4847f24704eeca6592ba85755a3e0
Jacek Lewandowski <lewandowski.jacek@gmail.com>
2015-02-18 08:01:40 +0100

    SPARKC-52: Support for native Cassandra count
    With this patch:
    - Added one more trait (SelectableColumnRef) in ColumnRef hierarchy to reflect columns without names
    - Added CountColumn case class
    - Refactored CassandraRDD, CassandraJavaRDD and CassandraJavaPairRDD
    - added test cases
    - CHANGES.txt

--- spark-cassandra-connector/src/it/scala/com/datastax/spark/connector/rdd/CassandraRDDSpec.scala
+++ spark-cassandra-connector/src/it/scala/com/datastax/spark/connector/rdd/CassandraRDDSpec.scala
@@ -581,4 +581,14 @@ class CassandraRDDSpec extends FlatSpec with Matchers with SharedEmbeddedCassand
     results.head.ttlOfValue <= ttl
   }
 
+  it should "count the CassandraRDD items" in {
+    val result = sc.cassandraTable("read_test", "big_table").count()
+    result shouldBe bigTableRowCount
+  }
+
+  it should "count the CassandraRDD items with where predicate" in {
+    val result = sc.cassandraTable("read_test", "big_table").where("key=1").count()
+    result shouldBe 1
+  }
+
 }
